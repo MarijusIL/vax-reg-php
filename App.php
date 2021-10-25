eb026f68ffd85dc697ff3d03e7b095952782afd8
MarijusIL <78425010+MarijusIL@users.noreply.github.com>
2021-10-25 16:02:15 +0300

    begun adding navigation logic

--- App.php
+++ App.php
@@ -7,12 +7,33 @@ Use Marijus\VaxReg\Controllers\DatabaseController;
 Use Marijus\VaxReg\Controllers\MessageController;
 
 class App {
+    private static $routes = ['!add', '!edit', '!delete', '!print', '!help', '!quit'];
 
     public static function start() {
-        echo 'app test ok' . "\r\n";
-        Appointment::test();
-        AppointmentController::test();
-        DatabaseController::test();
-        MessageController::test();
+        MessageController::greet();
+        $line = readline('Enter command: ');
+        self::router($line);
+    }
+
+    public static function getRoutes() {
+        return self::$routes;
+    }
+
+    public static function router(string $route) {
+        if ($route === '!help') {
+            MessageController::printMenu(self::$routes);
+        } elseif ($route === '!add') {
+
+        } elseif ($route === '!edit') {
+            
+        } elseif ($route === '!delete') {
+            
+        } elseif ($route === '!print') {
+            
+        } elseif ($route === '!quit') {
+
+        } else {
+            
+        }
     }
 }
--- controllers/MessageController.php
+++ controllers/MessageController.php
@@ -1,9 +1,23 @@
 <?php
 namespace Marijus\VaxReg\Controllers;
 
+use Marijus\VaxReg\App;
+
 class MessageController {
+    private static $divider = '___________________________' . "\r\n";
+    private static $greeting = 'Welcome to VaxReg' . "\r\n";
+    private static $menu = 'Input command or type !help for list of commands' . "\r\n";
     
-    public static function test() {
-        echo 'Message ctrler test ok' . "\r\n";
+    public static function greet() {
+        echo self::$divider;
+        echo self::$greeting;
+        echo self::$menu;
     }
+    public static function printMenu($routeArr) {
+        echo 'available commands are:' . "\r\n";
+        foreach ($routeArr as $route) {
+            echo $route . "\r\n";
+        }
+    }
+
 }
